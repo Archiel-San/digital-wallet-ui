86493fc251de84467fb8a9acde844da7bd8e615f
archiel <archielcruzdossantos@gmail.com>
2026-06-25 17:38:26 +0200

    feat: API client retries with new fresh tokens after error 401

--- shared/src/commonMain/kotlin/com/digitalwallet/app/network/ApiClient.kt
+++ shared/src/commonMain/kotlin/com/digitalwallet/app/network/ApiClient.kt
@@ -2,7 +2,6 @@ package com.digitalwallet.app.network
 
 import com.digitalwallet.app.data.local.TokenStorage
 import com.digitalwallet.app.data.model.AuthTokens
-import digitalwalletapp.shared.generated.resources.Res
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -25,7 +24,7 @@ object ApiClient {
     // iOS simulator   → localhost works directly
     //const val BASE_URL = "http://10.0.2.2:8085"
 
-    const val BASE_URL =  "https://2bcf-2605-59ca-86bc-6e08-5109-add4-4224-b9b4.ngrok-free.app"
+    const val BASE_URL =  "https://04f3-2605-59ca-86bc-6e08-c078-e84c-ecc6-8934.ngrok-free.app"
     val client = HttpClient {
         install(ContentNegotiation){
             json(Json{ignoreUnknownKeys= true})
@@ -70,6 +69,53 @@ object ApiClient {
         }
     }
 
+    suspend inline fun< reified T> postWithAuth(
+        path: String,
+        body: Any,
+        tokenStorage: TokenStorage
+    ): Result<T>{
+
+        return try {
+            val token = tokenStorage.getTokens()?.accessToken
+                ?: return Result.failure(Exception("Not Logged In"))
+
+            var response = client.post("$BASE_URL$path"){
+                bearerAuth(token)
+                contentType(ContentType.Application.Json)
+                setBody(body)
+            }
+
+            //TODO analisar why it gives me 401 when transfering when it should referesh automatically
+            // todo 0 descobri que ele bate 2x no transfer ao inves de ser uma vez no transfer (dar 401) depois no refresh
+            if(response.status == HttpStatusCode.Unauthorized){
+                val refreshed = refreshTokens(tokenStorage)
+                if (refreshed == null){
+                    tokenStorage.clearTokens()
+                    return Result.failure(Exception("Session Expired"))
+                }
+
+                response = client.post("$BASE_URL$path") {
+                    bearerAuth(refreshed.accessToken)
+                    contentType(ContentType.Application.Json)
+                    setBody(body)
+                }
+            }
+
+            if (!response.status.isSuccess()){
+                return Result.failure(Exception("Request Failed ${response.status}"))
+            }
+
+            Result.success(response.body())
+
+        }catch (e: Exception){
+            Result.failure(e)
+        }
+        
+    }
+
+
+
+    // TODO ver aqui o pq dele estar a bater duas vezes no mesmo endpoint e negar da segunda vez, apesar de ja ter token valido
     suspend fun refreshTokens(tokenStorage: TokenStorage): AuthTokens? {
         return try {
             val current = tokenStorage.getTokens()?: return null
